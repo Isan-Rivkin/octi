49f83d35c4b73471cecf9d09abf66975f34e1b62
Isan-Rivkin <isanrivkin@gmail.com>
2019-04-04 22:27:21 +0300

    initial module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod manifest; 
+use manifest::test;
 fn main() {
+    test();
     println!("Hello, world!");
 }
---
+++ src/manifest/mod.rs
@@ -0,0 +1,7 @@
+#[derive(Copy,Clone,Debug)]
+pub struct Manifest{
+    
+}
+pub fn test(){
+    println!("test func");
+}
